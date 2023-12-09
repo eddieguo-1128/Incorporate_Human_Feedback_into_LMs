a5c98d2ef2d6a2b3a4889d429487ac0d17915664
Jingyuan He <74093691+JingyuanHe1222@users.noreply.github.com>
2023-12-08 21:58:01 -0500

    Update rlhf_ppo.py
    update for the last dpo run

--- rlhf_ppo.py
+++ rlhf_ppo.py
@@ -1,79 +1,163 @@
+from dataclasses import dataclass, field
+from typing import Dict, Optional
+
 import torch
-from datasets import load_dataset
-from tqdm import tqdm
+from datasets import Dataset, load_dataset
+from transformers import AutoModelForCausalLM, AutoTokenizer, HfArgumentParser, TrainingArguments
 
-from transformers import AutoTokenizer, pipeline
-
-from trl import AutoModelForCausalLMWithValueHead, PPOConfig, PPOTrainer
+from trl import DPOTrainer
 
 
-dataset = load_dataset("Dahoas/full-hh-rlhf", split="train")
-dataset = dataset.rename_column("prompt", "query")
-dataset = dataset.remove_columns(["response", "chosen", "rejected"])
+# Define and parse arguments.
+@dataclass
+class ScriptArguments:
+    """
+    The arguments for the DPO training script.
+    """
 
-config = PPOConfig(
-    model_name="agi-css/hh-rlhf-sft",
-    learning_rate=1.41e-5,
-)
+    # data parameters
+    beta: Optional[float] = field(default=0.1, metadata={"help": "the beta parameter for DPO loss"})
+
+    # training parameters
+    default_model = "RaushanTurganbay/GPT2_instruct_tuned" # "agi-css/hh-rlhf-sft"
+    model_name_or_path: Optional[str] = field(default=default_model, metadata={"help": "the model name"})
+    learning_rate: Optional[float] = field(default=1.41e-5, metadata={"help": "optimizer learning rate"})
+    per_device_train_batch_size: Optional[int] = field(default=1, metadata={"help": "batch size per device"})
+    gradient_accumulation_steps: Optional[int] = field(
+        default=1, metadata={"help": "the number of gradient accumulation steps"}
+    )
+    max_length: Optional[int] = field(default=512, metadata={"help": "max length of each sample"})
+    max_prompt_length: Optional[int] = field(default=128, metadata={"help": "max length of each sample's prompt"})
+    max_target_length: Optional[int] = field(
+        default=128, metadata={"help": "Only used for encoder decoder model. Max target of each sample's prompt"}
+    )
+    label_pad_token_id: Optional[int] = field(default=-100, metadata={"help": "label for non response tokens"})
+    max_steps: Optional[int] = field(default=2000, metadata={"help": "max number of training steps"})
+    # instrumentation
+    sanity_check: Optional[bool] = field(default=True, metadata={"help": "only train on 1000 samples"})
+    report_to: Optional[str] = field(
+        default=None,
+        metadata={
+            "help": 'The list of integrations to report the results and logs to. Supported platforms are `"azure_ml"`,'
+            '`"comet_ml"`, `"mlflow"`, `"neptune"`, `"tensorboard"`,`"clearml"` and `"wandb"`. '
+            'Use `"all"` to report to all integrations installed, `"none"` for no integrations.'
+        },
+    )
+    # debug argument for distributed training
+    ignore_bias_buffers: Optional[bool] = field(
+        default=False,
+        metadata={
+            "help": "fix for DDP issues with LM bias/mask buffers - invalid scalar type,`inplace operation. See"
+            "https://github.com/huggingface/transformers/issues/22482#issuecomment-1595790992"
+        },
+    )
+    gradient_checkpointing: Optional[bool] = field(
+        default=False, metadata={"help": "Whether to use gradient checkpointing or no"}
+    )
+    gradient_checkpointing_kwargs: Optional[dict] = field(
+        default=None,
+        metadata={
+            "help": "key word arguments to be passed along `torch.utils.checkpoint.checkpoint` method - e.g. `use_reentrant=False`"
+        },
+    )
 
 
-tokenizer = AutoTokenizer.from_pretrained(config.model_name)
-tokenizer.pad_token = tokenizer.eos_token
-
-model = AutoModelForCausalLMWithValueHead.from_pretrained(config.model_name)
-
-next(model.parameters()).is_cuda
-
-
-# rm_tokenizer = AutoTokenizer.from_pretrained("weqweasdas/hh_rlhf_rm_open_llama_3b")
-
-rm_pipe = pipeline(
-    "sentiment-analysis",
-    model="weqweasdas/hh_rlhf_rm_open_llama_3b",
-    device="cuda",
-    tokenizer=tokenizer, #rm_tokenizer
-    model_kwargs={"torch_dtype": torch.bfloat16}
-)
-
-def tokenize(sample):
-    sample["input_ids"] = tokenizer.encode(sample["query"])
-    return sample
+def extract_anthropic_prompt(prompt_and_response):
+    """Extract the anthropic prompt from a prompt and response pair."""
+    search_term = "\n\nAssistant:"
+    search_term_idx = prompt_and_response.rfind(search_term)
+    assert search_term_idx != -1, f"Prompt and response does not contain '{search_term}'"
+    return prompt_and_response[: search_term_idx + len(search_term)]
 
 
-dataset = dataset.map(tokenize, batched=False)
+def get_hh(split: str, sanity_check: bool = False, silent: bool = False, cache_dir: str = None) -> Dataset:
+    """Load the Anthropic Helpful-Harmless dataset from Hugging Face and convert it to the necessary format.
+
+    The dataset is converted to a dictionary with the following structure:
+    {
+        'prompt': List[str],
+        'chosen': List[str],
+        'rejected': List[str],
+    }
+
+    Prompts should be structured as follows:
+      \n\nHuman: <prompt>\n\nAssistant:
+    Multiple turns are allowed, but the prompt should always start with \n\nHuman: and end with \n\nAssistant:.
+    """
+    dataset = load_dataset("Anthropic/hh-rlhf", split=split, cache_dir=cache_dir)
+    if sanity_check:
+        dataset = dataset.select(range(min(len(dataset), 1000)))
+
+    def split_prompt_and_responses(sample) -> Dict[str, str]:
+        prompt = extract_anthropic_prompt(sample["chosen"])
+        return {
+            "prompt": prompt,
+            "chosen": sample["chosen"][len(prompt) :],
+            "rejected": sample["rejected"][len(prompt) :],
+        }
+
+    return dataset.map(split_prompt_and_responses)
 
 
+if __name__ == "__main__":
+    parser = HfArgumentParser(ScriptArguments)
+    script_args = parser.parse_args_into_dataclasses()[0]
+    
+    print("init")
 
-ppo_trainer = PPOTrainer(
-    model=model,
-    config=config,
-    dataset=dataset, # no train_dataset param
-    tokenizer=tokenizer,
-)
-pipe_kwargs = {
-    "return_all_scores": True,
-    "function_to_apply": "none",
-    "batch_size": 1
-}
+    # LM fine tuned on the dataset ready for dpo
+    model = AutoModelForCausalLM.from_pretrained(script_args.model_name_or_path)
+    # reference model we have for comparison
+    model_ref = AutoModelForCausalLM.from_pretrained(script_args.model_name_or_path)
 
+    tokenizer = AutoTokenizer.from_pretrained(script_args.model_name_or_path)
+    if tokenizer.pad_token is None:
+        tokenizer.pad_token = tokenizer.eos_token
 
-for epoch, batch in tqdm(enumerate(ppo_trainer.dataloader)):
-    query_tensors = batch["input_ids"]
+    # 2. Load the Anthropic Helpful-Harmless dataset
+    train_dataset = get_hh("train", sanity_check=script_args.sanity_check)
 
-    #### Get response from SFTModel
-    response_tensors = ppo_trainer.generate(query_tensors, **pipe_kwargs)
-    batch["response"] = [tokenizer.decode(r.squeeze()) for r in response_tensors]
+    # 3. Load evaluation dataset
+    eval_dataset = get_hh("test", sanity_check=script_args.sanity_check)
 
-    #### Compute reward score
-    texts = [q + r for q, r in zip(batch["query"], batch["response"])]
-    # pipe_outputs = reward_model(texts)
-    # rewards = [torch.tensor(output[1]["score"]) for output in pipe_outputs]
-    pipe_outputs = rm_pipe(texts, **pipe_kwargs)
-    rewards = [output[0]["score"] for output in pipe_outputs]
+    # 4. initialize training arguments:
+    training_args = TrainingArguments(
+        per_device_train_batch_size=script_args.per_device_train_batch_size,
+        max_steps=script_args.max_steps,
+        remove_unused_columns=False,
+        gradient_accumulation_steps=script_args.gradient_accumulation_steps,
+        learning_rate=script_args.learning_rate,
+        evaluation_strategy="steps",
+        logging_first_step=True,
+        logging_steps=10,  # match results in blog post
+        eval_steps=500,
+        output_dir="./test",
+        optim="rmsprop",
+        warmup_steps=150,
+        report_to=script_args.report_to,
+        bf16=False,
+        gradient_checkpointing=script_args.gradient_checkpointing,
+    )
 
-    #### Run PPO step
-    stats = ppo_trainer.step(query_tensors, response_tensors, rewards)
-    ppo_trainer.log_stats(stats, batch, rewards)
+    # 5. initialize the DPO trainer
+    dpo_trainer = DPOTrainer(
+        model,
+        model_ref,
+        args=training_args,
+        beta=script_args.beta,
+        train_dataset=train_dataset,
+        eval_dataset=eval_dataset,
+        tokenizer=tokenizer,
+        max_length=script_args.max_length,
+        max_target_length=script_args.max_target_length,
+        max_prompt_length=script_args.max_prompt_length,
+        generate_during_eval=True,
+    )
 
-#### Save model
-ppo_trainer.save_model("hhrlhf_ppo")
+    # 6. train
+    dpo_trainer.train()
+    
+    
+    
+    # save at checkpoint already
+    
